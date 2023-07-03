a75bd812ff22c707db6404f6fe45d117be7d18e3
oluobamzy <oluobamzy@gmail.com>
2023-07-03 15:27:23 -0400

    tail function refactored with tail test

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,13 +1,13 @@
-const assertEqual = require('../assertEqual');
+const assert = require('chai').assert;
 const tail = require('../tail');
-// TEST CODE
 
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result.length, 2);
-assertEqual(result[0], "Lighthouse");
-assertEqual(result[1], "Labs");
-/*assertEqual(result, ["Lighthouse", "Labs"]);//=> will always fail!
-//TestCase:Check the original array */
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-//tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3); // original array should still have 3 elements!
+describe("#tail",()=>{
+  it("['Hello'] should return undefined",()=>{
+    assert.strictEqual(tail(['Hello']),undefined);
+  });
+  it("['Hello','Lighthouse','Labs'] should return ['lighthouse','Labs']",()=>{
+    assert.deepEqual(tail(['Hello','Lighthouse','Labs']),['Lighthouse','Labs']);
+  });
+});
+
+
