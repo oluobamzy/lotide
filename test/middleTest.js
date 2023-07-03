f578d67d1cbd3bfbb109c8eff26f190b34bf112a
oluobamzy <oluobamzy@gmail.com>
2023-07-03 14:47:25 -0400

    middle function refactored to use mocha test

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,4 +1,9 @@
-const assertArraysEqual = require('../assertArraysEqual');
+const assert = require('chai').assert;
 const middle = require('../middle');
-console.log(assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]));
+describe("#middle",()=>{
+  it("[1,2,3,4,5,6] should return [3,4]",()=>{
+    assert.deepEqual(middle([1,2,3,4,5,6]),[3,4]);
+  });
+});
+
 
