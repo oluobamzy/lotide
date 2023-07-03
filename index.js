c40a2d81211e37cf35f9577a77de2a726b5b0b05
oluobamzy <oluobamzy@gmail.com>
2023-07-03 15:50:07 -0400

    adding utility functions and exporting lotide

---
+++ index.js
@@ -0,0 +1,17 @@
+const head   = require('./head');
+const tail   = require('./tail');
+const middle = require('./middle');
+const eqArrays = require('./eqArrays');
+const without = require('./without');
+const takeUntil = require('./takeUntil');
+const findKeyByValue = require('./findKeyByValue');
+const findKey = require('./findKey')
+const eqObjects = require('./eqObjects');
+const countOnly = require('./countOnly');
+
+module.exports = {
+  head:   head,
+  tail:   tail,
+  middle: middle,
+  eqArrays:eqArrays
+};
