720a116b47c8f8c46443d36a9b4ed1d0d130dac9
oluobamzy <oluobamzy@gmail.com>
2023-07-03 13:42:01 -0400

    head and assertEqual refactored

--- head.js
+++ head.js
@@ -1,7 +1,4 @@
-const assertEqual = function(actual, expected) {
-  actual === expected ? console.log(`✔✔✔Assertion Passed: [${actual}] === [${expected}]`) : console.log(`❌❌❌Assertion Failed: [${actual}] !== [${expected}]`);
-};
-
+const assertEqual = require('./assertEqual')
 const head = function(actual){
   
   if(actual.length > 1 || actual.length === 1){
@@ -10,8 +7,4 @@ const head = function(actual){
   return undefined;
 }
 
-//TEST CODE
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head([5]), 5);
-assertEqual(head([]), 5);
+module.exports = head;
